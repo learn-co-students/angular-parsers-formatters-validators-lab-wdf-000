8febb3f360cfc523e0ab6a7a6e70fc853a7f2eb1
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-28 16:52:01 -0400

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,7 +1,15 @@
 function couponCode() {
-
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModel) {
+      ngModel.$validators.couponCode = function(str) {
+	return str.match(/\d{2}[a-z]{4}\d{2}/i);
+      }
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('couponCode', couponCode);
+  .module('app')
+  .directive('couponCode', couponCode);
--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,7 +1,15 @@
 function makeLower() {
-	
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function (str) {
+	return str.toLowerCase();
+      });
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('makeLower', makeLower);
+  .module('app')
+  .directive('makeLower', makeLower);
